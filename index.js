a785db88a7339dbeb9ffd38843f826e643cbd244
github username <github email address>
2017-12-25 09:42:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const produceDrivingRange = function(blockRange) {
   return function(start, end) {
-    const distance = Math.abs(start.slice(0,2) - end.slice(0,2));
+    const distance = Math.abs(start.replace(/\D+/, '') - end.replace(/\D+/, ''));
     const difference = blockRange - distance;
     if (difference >= 0) {
       return `within range by ${difference}`;
